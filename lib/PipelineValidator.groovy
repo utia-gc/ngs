ff691735be15ea37249bc09e6914a5ff8cac69be
Trevor F. Freeman <tfreem10@vols.utk.edu>
2024-01-25 13:25:05 -0500

    feat: validate map tools

--- lib/PipelineValidator.groovy
+++ lib/PipelineValidator.groovy
@@ -14,6 +14,7 @@ static void validateRequiredParams(params, log) {
     validateGenome(params, log)
     validateAnnotations(params, log)
     validateTrimTool(params, log)
+    validateMapTool(params, log)
 }
 
 
@@ -118,6 +119,32 @@ static void validateTrimTool(params, log) {
         }
     } else {
         log.error "Parameter 'tools.trim' is required but was not provided."
+        System.exit(64)
+    }
+}
+
+
+/**
+ * Validate map tool.
+ *
+ * Check that map tool param exists and is a valid map tool.
+ * Throw exit status 64 if otherwise.
+ *
+ * @param params The params for the Nextflow pipeline.
+ * @param log The Nextflow log object.
+ *
+ * @return null
+ */
+static void validateMapTool(params, log) {
+    if (params.tools.map) {
+        if (Tools.Map.isMapTool(params.tools.map)) {
+            log.info "Using map tool '${params.tools.map}'"
+        } else {
+            log.error "'${params.tools.map}' is not a valid map tool."
+            System.exit(64)
+        }
+    } else {
+        log.error "Parameter 'tools.map' is required but was not provided."
         System.exit(64)
     }
 }
