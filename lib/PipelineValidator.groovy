9d6495cd811f105f0955d46c28ca2ed4fe5f4e9c
Trevor F. Freeman <tfreem10@vols.utk.edu>
2024-06-26 14:11:14 -0400

    refactor: require genome and annotations with nf-schema

--- lib/PipelineValidator.groovy
+++ lib/PipelineValidator.groovy
@@ -10,8 +10,6 @@
  */
 static void validateRequiredParams(params, log) {
     validateSamplesheet(params, log)
-    validateGenome(params, log)
-    validateAnnotations(params, log)
     validateTrimTool(params, log)
     validateMapTool(params, log)
 }
@@ -32,46 +30,6 @@ static void validateSamplesheet(params, log) {
         log.info "Using samplesheet '${params.samplesheet}'"
     } else {
         log.error "Parameter 'samplesheet' is required but was not provided."
-        System.exit(64)
-    }
-}
-
-
-/**
- * Validate genome
- *
- * Check that genome param exists. Throw exit status 64 if it doesn't.
- *
- * @param params The params for the Nextflow pipeline.
- * @param log The Nextflow log object.
- *
- * @return null
- */
-static void validateGenome(params, log) {
-    if (params.genome) {
-        log.info "Using reference genome '${params.genome}'"
-    } else {
-        log.error "Parameter 'genome' is required but was not provided."
-        System.exit(64)
-    }
-}
-
-
-/**
- * Validate annotations
- *
- * Check that annotations param exists. Throw exit status 64 if it doesn't.
- *
- * @param params The params for the Nextflow pipeline.
- * @param log The Nextflow log object.
- *
- * @return null
- */
-static void validateAnnotations(params, log) {
-    if (params.annotations) {
-        log.info "Using reference annotations '${params.annotations}'"
-    } else {
-        log.error "Parameter 'annotations' is required but was not provided."
         System.exit(64)
     }
 }
