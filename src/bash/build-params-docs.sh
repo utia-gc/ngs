3c9529f0f3c58ba538dd77d03ee381f25ebc98c7
Trevor F. Freeman <tfreem10@vols.utk.edu>
2025-12-17 13:23:25 -0500

    build(deps): use nf-core pipelines schema command

--- src/bash/build-params-docs.sh
+++ src/bash/build-params-docs.sh
@@ -3,7 +3,8 @@
 readonly params_docs_path="docs/pipeline_config/params.md"
 
 # build the docs page from the nf-schema (nf-validate) nextflow_schema.json file
-nf-core schema docs --output "${params_docs_path}" --force
+. .venv/bin/activate
+nf-core pipelines schema docs --output "${params_docs_path}" --force
 
 # replace the params docs file header with just "Params"
 sed -i 's|utia-gc/ngs pipeline parameters|Params|g' "${params_docs_path}"
